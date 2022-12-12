13bd910db5e794796e0b63e1d1595513afb94375
Neil Tucker <49943805+neiltucker@users.noreply.github.com>
2022-12-12 12:43:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     orders = pd.read_csv('orderdata.csv')
-    return orders.to_html()
+    return orders.to_html(index=False)
 
 # Run the app if it is the main module
 if __name__ == '__main__':
